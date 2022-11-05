bc5f1f182ee8088f7a2d5f8f44234d403494f79c
Sheenor101 <sheena_walsh@hotmail.com>
2022-11-05 22:32:23 +0000

    Birdy3

--- birdy.cpp
+++ birdy.cpp
@@ -1,25 +1,43 @@
 // Include the raylib library resource
 #include "raylib.h"
+
+struct Anim
+{ Rectangle rec;
+  Vector2 pos;
+  int frame;
+  float updateTime;
+  float runningTime;
+  };
+
+
 //ENTRY POINT TO GET INTO cpp CODING
 int main(){
 // declaring the variables for the window size
  const int windowWidth{700};
  const int windowHeight{450};
+
+
+	
+
+ 
  // Array to use more than one character
- char WindowName[] = "birdvert";
+ char WindowName[] = "Birdy";
+
+ 
  //function to create a window with the above values
 
- int rectangle_posX = 700;
- int rectangle_posY = 100;
+ // int rectangle_posX = 700;
+ // int rectangle_posY = 100;
  
 
 
 InitWindow(windowWidth,windowHeight,WindowName);
 
 
-Texture2D sky = LoadTexture("resources/sky.png");
 
-Texture2D birdvert = LoadTexture ("resources/birdvert.png");
+Texture2D sky = LoadTexture("resources/sky.png");
+Texture2D birdy = LoadTexture ("resources/birdy.png");
+
 
 Rectangle skyRec;
 skyRec.width = sky.width*10;
@@ -30,25 +48,101 @@ Vector2 skyPos;
 skyPos.x = windowWidth/2 - skyRec.width/2;
 skyPos.y = windowHeight - skyRec.height;
 
-Rectangle birdvertRec;
-birdvertRec.width = birdvert.width;
-birdvertRec.height = birdvert.height/6;
-birdvertRec.x = 0;
-birdvertRec.y = 0;
-Vector2 birdvertPos;
-birdvertPos.x = windowWidth/2 - birdvertRec.width;
-birdvertPos.y = windowHeight/2 - birdvertRec.height/6;
+Anim birdyAnim;
+birdyAnim.rec.width = birdy.width/6;
+birdyAnim.rec.height = birdy.height;
+birdyAnim.rec.x = 0;
+birdyAnim.rec.y = 0;
+birdyAnim.pos.x = windowWidth/2 - birdyAnim.rec.width/2;
+birdyAnim.pos.y = windowHeight - birdyAnim.rec.height;
+birdyAnim.frame = 0;
+birdyAnim.updateTime = 1.0/12.0;
+birdyAnim.runningTime = 0.0;
 
+
+int velocity{0};
+const int jumpHeight{500};
+bool jumped{false};
+int speed{200};
+bool collision{};
 
 // Setting the frame per second rate of the game
 SetTargetFPS(60);
 
 // loop to keep window open until x button is pressed
 while (WindowShouldClose() == false){
+		const float deltaTime{GetFrameTime()};
 
+	Rectangle birdyRec{
+	birdyAnim.pos.x,
+	birdyAnim.pos.y,
+	birdyAnim.rec.height,
+	birdyAnim.rec.width	
+	};
 
-		
+	if(IsKeyDown(KEY_D)&& !jumped)
+{
+	birdyAnim.pos.x += speed*deltaTime;
+	birdyAnim.rec.width = birdy.width/6;
+
+	birdyAnim.runningTime += deltaTime; 
+	if(birdyAnim.runningTime >= birdyAnim.updateTime)
+	{
+	birdyAnim.runningTime = 0.0;
+	birdyAnim.rec.x = birdyAnim.frame* birdyAnim.rec.width;
+	birdyAnim.frame++;
+	if (birdyAnim.frame>5){
+	birdyAnim.frame = 0;
+	}
+
+	}
+}
+if(IsKeyReleased(KEY_D)&& !jumped)
+{birdyAnim.frame = 0;
+birdyAnim.rec.x = birdyAnim.frame* birdyAnim.rec.width;}
+
+if(IsKeyDown(KEY_A)&& !jumped)
+{
+	birdyAnim.pos.x -= speed*deltaTime;
+	birdyAnim.rec.width = -birdy.width/6;
+
+	birdyAnim.runningTime += deltaTime; 
+	if(birdyAnim.runningTime >= birdyAnim.updateTime)
+	{
+	birdyAnim.runningTime = 0.0;
+	birdyAnim.rec.x = birdyAnim.frame* birdyAnim.rec.width;
+	birdyAnim.frame++;
+	if (birdyAnim.frame>5){
+	birdyAnim.frame = 0;
+	}
+
+	}
+}
+if(IsKeyReleased(KEY_A)&& !jumped)
+{birdyAnim.frame = 0;
+birdyAnim.rec.x = birdyAnim.frame* birdyAnim.rec.width;}
+
 BeginDrawing();
+
+//if (posY >= windowHeight - height){
+if (birdyAnim.pos.y >= windowHeight - birdy.height){
+
+	velocity=0;
+	jumped = false; 
+}
+else 
+{velocity += gravity * deltaTime;
+jumped = true;
+}
+if(IsKeyPressed(KEY_SPACE) && !jumped)
+{
+	velocity -= jumpHeight;
+}
+
+//posY += velocity;
+birdyAnim.pos.y += velocity * deltaTime; 
+obPos.x += obVel * deltaTime; 
+
 
 ClearBackground(SKYBLUE);
 
@@ -56,21 +150,20 @@ DrawTextureRec(sky, skyRec,skyPos, WHITE);
 skyPos.x -= 1;
 
 
-DrawTextureRec(birdvert,birdvertRec,birdvertPos,WHITE);
+DrawTextureRec(birdy,birdyAnim.rec,birdyAnim.pos,WHITE);
 
 
 
 
 
-DrawRectangle(rectangle_posX, rectangle_posY, 10, 5, LIGHTGRAY);
-rectangle_posX -= 5;
+// DrawRectangle(rectangle_posX, rectangle_posY, 10, 5, BLACK);
+// rectangle_posX -= 5;
 
 
 
 EndDrawing();
 }
-// Unload Textures to save memory
-UnloadTexture(sky);
-UnloadTexture(birdvert);
+UnloadTexture (birdy);
+UnloadTexture (sky);
 CloseWindow();
 }
