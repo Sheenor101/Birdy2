bd518534dee0a0cfcb3f141d09e41594f1b4985e
Sheenor101 <sheena_walsh@hotmail.com>
2022-11-05 13:10:57 +0000

    Birdy5

--- birdy.cpp
+++ birdy.cpp
@@ -6,10 +6,9 @@ int main(){
  const int windowWidth{700};
  const int windowHeight{450};
  // Array to use more than one character
- char WindowName[] = "Birdy";
+ char WindowName[] = "birdvert";
  //function to create a window with the above values
- int circle_posX = 250;
- int circle_posY = 250;
+
  int rectangle_posX = 700;
  int rectangle_posY = 100;
  
@@ -17,22 +16,35 @@ int main(){
 
 InitWindow(windowWidth,windowHeight,WindowName);
 
+
 Texture2D sky = LoadTexture("resources/sky.png");
 
+Texture2D birdvert = LoadTexture ("resources/birdvert.png");
+
 Rectangle skyRec;
-skyRec.width = sky.width/6;
+skyRec.width = sky.width*10;
 skyRec.height = sky.height;
 skyRec.x = 0;
-skyRec.y = 0; 
+skyRec.y = 0;
 Vector2 skyPos;
 skyPos.x = windowWidth/2 - skyRec.width/2;
 skyPos.y = windowHeight - skyRec.height;
+
+Rectangle birdvertRec;
+birdvertRec.width = birdvert.width;
+birdvertRec.height = birdvert.height/6;
+birdvertRec.x = 0;
+birdvertRec.y = 0;
+Vector2 birdvertPos;
+birdvertPos.x = windowWidth/2 - birdvertRec.width;
+birdvertPos.y = windowHeight/2 - birdvertRec.height/6;
+
 
 // Setting the frame per second rate of the game
 SetTargetFPS(60);
 
 // loop to keep window open until x button is pressed
-	while (WindowShouldClose() == false){
+while (WindowShouldClose() == false){
 
 
 		
@@ -40,31 +52,25 @@ BeginDrawing();
 
 ClearBackground(SKYBLUE);
 
+DrawTextureRec(sky, skyRec,skyPos, WHITE);
+skyPos.x -= 1;
 
-DrawCircle(circle_posX,circle_posY,50, RED);
 
-if (IsKeyDown(KEY_W))
-{
-	circle_posY-=5;
-}
-if (IsKeyDown(KEY_S))
-{
-	circle_posY+=5;
-}
-if (IsKeyDown(KEY_A))
-{
-	circle_posX-=5;
-}
-if (IsKeyDown(KEY_D))
-{
-	circle_posX+=5;
-}
+DrawTextureRec(birdvert,birdvertRec,birdvertPos,WHITE);
+
+
+
+
 
 DrawRectangle(rectangle_posX, rectangle_posY, 10, 5, LIGHTGRAY);
 rectangle_posX -= 5;
 
-DrawTexture(sky,skyRec,skyPos, WHITE);
+
 
 EndDrawing();
 }
+// Unload Textures to save memory
+UnloadTexture(sky);
+UnloadTexture(birdvert);
+CloseWindow();
 }
