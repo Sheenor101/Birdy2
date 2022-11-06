7882be7e8e70bcc117c978ce810b040613bcfe74
Sheenor101 <sheena_walsh@hotmail.com>
2022-11-06 12:44:34 +0000

    Birdy With Can

--- birdy.cpp
+++ birdy.cpp
@@ -1,12 +1,22 @@
 // Include the raylib library resource
 #include "raylib.h"
 
+
+
 struct Anim
 { Rectangle rec;
   Vector2 pos;
   int frame;
   float updateTime;
   float runningTime;
+  };
+
+
+struct can
+{ Rectangle rec;
+  Vector2 speed;
+  bool active;
+  Color color;
   };
 
 
@@ -17,8 +27,6 @@ int main(){
  const int windowHeight{450};
 
 
-	
-
  
  // Array to use more than one character
  char WindowName[] = "Birdy";
@@ -26,18 +34,17 @@ int main(){
  
  //function to create a window with the above values
 
- // int rectangle_posX = 700;
- // int rectangle_posY = 100;
+ int rectangle_posX = 700;
+ int rectangle_posY = 100;
  
 
-
 InitWindow(windowWidth,windowHeight,WindowName);
-
+int canVel{-100};
 
 
 Texture2D sky = LoadTexture("resources/sky.png");
 Texture2D birdy = LoadTexture ("resources/birdy.png");
-
+Texture2D can = LoadTexture("resources/can.png");
 
 Rectangle skyRec;
 skyRec.width = sky.width*10;
@@ -60,11 +67,21 @@ birdyAnim.updateTime = 1.0/12.0;
 birdyAnim.runningTime = 0.0;
 
 
+Rectangle canRec;
+canRec.width = can.width;
+canRec.height = can.height;
+canRec.x = 0;
+canRec.y = 0;
+Vector2 canPos;
+canPos.x = windowWidth- can.width;
+canPos.y = windowHeight - can.height;
+
+
 int velocity{0};
-const int jumpHeight{500};
-bool jumped{false};
+
+
 int speed{200};
-bool collision{};
+
 
 // Setting the frame per second rate of the game
 SetTargetFPS(60);
@@ -73,14 +90,9 @@ SetTargetFPS(60);
 while (WindowShouldClose() == false){
 		const float deltaTime{GetFrameTime()};
 
-	Rectangle birdyRec{
-	birdyAnim.pos.x,
-	birdyAnim.pos.y,
-	birdyAnim.rec.height,
-	birdyAnim.rec.width	
-	};
 
-	if(IsKeyDown(KEY_D)&& !jumped)
+
+	if(IsKeyDown(KEY_D))
 {
 	birdyAnim.pos.x += speed*deltaTime;
 	birdyAnim.rec.width = birdy.width/6;
@@ -97,11 +109,12 @@ while (WindowShouldClose() == false){
 
 	}
 }
-if(IsKeyReleased(KEY_D)&& !jumped)
-{birdyAnim.frame = 0;
-birdyAnim.rec.x = birdyAnim.frame* birdyAnim.rec.width;}
+	if(IsKeyReleased(KEY_D))
+	{birdyAnim.frame = 1;
+	birdyAnim.rec.x = birdyAnim.frame* birdyAnim.rec.width;}
 
-if(IsKeyDown(KEY_A)&& !jumped)
+
+if(IsKeyDown(KEY_A))
 {
 	birdyAnim.pos.x -= speed*deltaTime;
 	birdyAnim.rec.width = -birdy.width/6;
@@ -118,46 +131,72 @@ if(IsKeyDown(KEY_A)&& !jumped)
 
 	}
 }
-if(IsKeyReleased(KEY_A)&& !jumped)
-{birdyAnim.frame = 0;
+if(IsKeyReleased(KEY_A))
+{birdyAnim.frame = 2;
 birdyAnim.rec.x = birdyAnim.frame* birdyAnim.rec.width;}
+
+
+
+if(IsKeyDown(KEY_W))
+{
+	birdyAnim.pos.y -= speed*deltaTime;
+	birdyAnim.rec.width = -birdy.width/6;
+
+	birdyAnim.runningTime += deltaTime; 
+	if(birdyAnim.runningTime >= birdyAnim.updateTime)
+	{
+	birdyAnim.runningTime = 0.0;
+	birdyAnim.rec.x = birdyAnim.frame* birdyAnim.rec.width;
+	birdyAnim.frame++;
+	if (birdyAnim.frame>5){
+	birdyAnim.frame = 0;
+	}
+
+	}
+}
+if(IsKeyReleased(KEY_W))
+{birdyAnim.frame = 3;
+birdyAnim.rec.x = birdyAnim.frame* birdyAnim.rec.width;}
+
+if(IsKeyDown(KEY_S))
+{
+	birdyAnim.pos.y += speed*deltaTime;
+	birdyAnim.rec.width = -birdy.width/6;
+
+	birdyAnim.runningTime += deltaTime; 
+	if(birdyAnim.runningTime >= birdyAnim.updateTime)
+	{
+	birdyAnim.runningTime = 0.0;
+	birdyAnim.rec.x = birdyAnim.frame* birdyAnim.rec.width;
+	birdyAnim.frame++;
+	if (birdyAnim.frame>5){
+	birdyAnim.frame = 0;
+	}
+
+	}
+}
+if(IsKeyReleased(KEY_S))
+{birdyAnim.frame = 4;
+birdyAnim.rec.x = birdyAnim.frame* birdyAnim.rec.width;}
+
 
 BeginDrawing();
 
-//if (posY >= windowHeight - height){
-if (birdyAnim.pos.y >= windowHeight - birdy.height){
 
-	velocity=0;
-	jumped = false; 
-}
-else 
-{velocity += gravity * deltaTime;
-jumped = true;
-}
-if(IsKeyPressed(KEY_SPACE) && !jumped)
-{
-	velocity -= jumpHeight;
-}
 
-//posY += velocity;
 birdyAnim.pos.y += velocity * deltaTime; 
-obPos.x += obVel * deltaTime; 
+canPos.x += canVel * deltaTime;
+
+
 
 
 ClearBackground(SKYBLUE);
 
-DrawTextureRec(sky, skyRec,skyPos, WHITE);
-skyPos.x -= 1;
-
-
+DrawTextureRec(sky, skyRec,skyPos, WHITE); skyPos.x -= 1;
 DrawTextureRec(birdy,birdyAnim.rec,birdyAnim.pos,WHITE);
-
-
-
-
-
-// DrawRectangle(rectangle_posX, rectangle_posY, 10, 5, BLACK);
-// rectangle_posX -= 5;
+DrawTextureRec(can,canRec,canPos,WHITE);
+DrawRectangle(rectangle_posX, rectangle_posY, 10, 5, BLACK);
+rectangle_posX -= 4;
 
 
 
@@ -165,5 +204,6 @@ EndDrawing();
 }
 UnloadTexture (birdy);
 UnloadTexture (sky);
+UnloadTexture (can);
 CloseWindow();
 }
