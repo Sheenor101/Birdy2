524a00e7ea7c9d62a423ea849659756763f22fa8
Sheenor101 <sheena_walsh@hotmail.com>
2022-11-08 20:09:18 +0000

    Birdy Obstacle

--- birdy.cpp
+++ birdy.cpp
@@ -1,47 +1,48 @@
-// Include the raylib library resource
 #include "raylib.h"
 
-
-
-//group
 struct Anim
-{ Rectangle rec;
-  Vector2 pos;
-  int frame;
-  //used to slow down fram
-  float updateTime;
-  float runningTime;
-  };
+{
+	Rectangle rec; 
+	Vector2 pos;
+	int frame; 
+	float updateTime; 
+	float runningTime;  
+};
 
 
 
-bool CheckCollisionRecCircle;
-
-
-
-//ENTRY POINT TO GET INTO cpp CODING
 int main(){
-
-// declaring the variables for the window size
  const int windowWidth{700};
  const int windowHeight{450};
-
-
- 
- // Array to use more than one character
- char WindowName[] = "Birdy";
-
- 
- //function to create a window with the above values
-
-
-InitWindow(windowWidth,windowHeight,WindowName);
+InitWindow(windowWidth,windowHeight,"Birdy");
 InitAudioDevice();
+//const int width{50};
+//const int height{80};
 
 
+Texture2D obstacle = LoadTexture("resources/Drone.png");
+Texture2D birdy = LoadTexture("resources/birdy.png");
 Texture2D sky = LoadTexture("resources/sky.png");
-Texture2D birdy = LoadTexture ("resources/birdy.png");
 
+Anim birdyAnim;
+birdyAnim.rec.width = birdy.width/6; 
+birdyAnim.rec.height = birdy.height;
+birdyAnim.rec.x = 0;
+birdyAnim.rec.y = 0;
+birdyAnim.pos.x = windowWidth/2 - birdyAnim.rec.width/2;
+birdyAnim.pos.y = windowHeight - birdyAnim.rec.height;
+birdyAnim.frame = 0;
+birdyAnim.updateTime = 1.0/12.0;
+birdyAnim.runningTime = 0.0;
+
+Rectangle obRec;
+obRec.width = obstacle.width;
+obRec.height = obstacle.height; 
+obRec.x = 0;
+obRec.y = 0;
+Vector2 obPos; 
+obPos.x = windowWidth- obRec.width;
+obPos.y = windowHeight - obRec.height;
 
 Rectangle skyRec;
 skyRec.width = sky.width*10;
@@ -52,54 +53,44 @@ Vector2 skyPos;
 skyPos.x = windowWidth/2 - skyRec.width/2;
 skyPos.y = windowHeight - skyRec.height;
 
-Anim birdyAnim;
-birdyAnim.rec.width = birdy.width/6;
-birdyAnim.rec.height = birdy.height;
-birdyAnim.rec.x = 0;
-birdyAnim.rec.y = 0;
-birdyAnim.pos.x = windowWidth/2 - birdyAnim.rec.width/2;
-birdyAnim.pos.y = windowHeight - birdyAnim.rec.height;
-birdyAnim.frame = 0;
-birdyAnim.updateTime = 1.0/12.0;
-birdyAnim.runningTime = 0.0;
 
 
 
+//int posY{windowHeight-height};
+int obVel{0};
 int velocity{0};
 
 
 int speed{400};
-
- Vector2 ballPos = { GetScreenWidth() / 2.0f, GetScreenHeight() / 2.0f };
-    Vector2 ballSpeed = { 8.0f, 6.0f };
-    float ballRad = 10;
-
-
-bool collision;
+bool collision{};
 
 Music music = LoadMusicStream("resources/Music.wav");
 PlayMusicStream(music);
 
-// Setting the frame per second rate of the game
-SetTargetFPS(60);
+SetTargetFPS(60); 
 
-// loop to keep window open until x button is pressed
-while (WindowShouldClose() == false){
-		const float deltaTime{GetFrameTime()};
+	while(!WindowShouldClose()){
+	const float deltaTime{GetFrameTime()};
 
 UpdateMusicStream(music);
 
-Rectangle birdyRec{
+	Rectangle obstacleRec{
+		obPos.x,
+		obPos.y,
+		obRec.height,
+		obRec.width,
+	};
+	Rectangle birdyRec{
 	birdyAnim.pos.x,
 	birdyAnim.pos.y,
 	birdyAnim.rec.height,
-	birdyAnim.rec.width
-};
+	birdyAnim.rec.width	
+	};
+	if(CheckCollisionRecs(birdyRec,obstacleRec)){
+		collision = true;
+	}
 
-// if (CheckCollisionRecs(birdyRec,ball)==false){
-// 		collision = true;
 
-// }
 
 	if(IsKeyDown(KEY_D))
 {
@@ -110,8 +101,6 @@ Rectangle birdyRec{
 	if(birdyAnim.runningTime >= birdyAnim.updateTime)
 	{
 	birdyAnim.runningTime = 0.0;
-
-	//birdyAnim.rec.x = birdyAnim.frame* birdyAnim.rec.width;
 	birdyAnim.rec.x = birdyAnim.frame* birdyAnim.rec.width;
 	birdyAnim.frame++;
 	if (birdyAnim.frame>5){
@@ -120,9 +109,9 @@ Rectangle birdyRec{
 
 	}
 }
-	if(IsKeyReleased(KEY_D))
-	{birdyAnim.frame = 1;
-	birdyAnim.rec.x = birdyAnim.frame* birdyAnim.rec.width;}
+if(IsKeyReleased(KEY_D))
+{birdyAnim.frame = 2;
+birdyAnim.rec.x = birdyAnim.frame* birdyAnim.rec.width;}
 
 
 if(IsKeyDown(KEY_A))
@@ -143,9 +132,8 @@ if(IsKeyDown(KEY_A))
 	}
 }
 if(IsKeyReleased(KEY_A))
-{birdyAnim.frame = 2;
+{birdyAnim.frame = 3;
 birdyAnim.rec.x = birdyAnim.frame* birdyAnim.rec.width;}
-
 
 
 if(IsKeyDown(KEY_W))
@@ -166,8 +154,10 @@ if(IsKeyDown(KEY_W))
 	}
 }
 if(IsKeyReleased(KEY_W))
-{birdyAnim.frame = 3;
+{birdyAnim.frame = 4;
 birdyAnim.rec.x = birdyAnim.frame* birdyAnim.rec.width;}
+
+
 
 if(IsKeyDown(KEY_S))
 {
@@ -187,44 +177,32 @@ if(IsKeyDown(KEY_S))
 	}
 }
 if(IsKeyReleased(KEY_S))
-{birdyAnim.frame = 4;
+{birdyAnim.frame = 5;
 birdyAnim.rec.x = birdyAnim.frame* birdyAnim.rec.width;}
-
- // Bouncing ball logic
-        ballPos.x += ballSpeed.x;
-        ballPos.y += ballSpeed.y;
-        if ((ballPos.x >= (GetScreenWidth() - ballRad)) || (ballPos.x <= ballRad)) ballSpeed.x *= -1.0f;
-        if ((ballPos.y >= (GetScreenHeight() - ballRad)) || (ballPos.y <= ballRad)) ballSpeed.y *= -1.0f;
-
 
 
 BeginDrawing();
 
+//if (posY >= windowHeight - height){
 
+
+
+//posY += velocity;
 birdyAnim.pos.y += velocity * deltaTime; 
+obPos.x += obVel * deltaTime; 
+ClearBackground(WHITE); 
+	if (collision){
 
-
-
-
-
-ClearBackground(SKYBLUE);
-
-
-//if (collision){DrawText("Game Over",300,50,20,DARKGRAY);
-
-//}
-//else{
-DrawText("Save Birdy from the missile!", 100, 100, 50, BLACK);
-DrawTextureRec(sky, skyRec,skyPos, WHITE); skyPos.x -= 1;
+	}
+	else{
+//DrawRectangle(windowWidth/2,posY,width,height,GREEN);
 DrawTextureRec(birdy,birdyAnim.rec,birdyAnim.pos,WHITE);
-DrawCircleV(ballPos, ballRad, DARKPURPLE);
-//}
-
-
-
+DrawTextureRec(obstacle,obRec,obPos,WHITE);
+DrawTextureRec(sky, skyRec,skyPos, WHITE); skyPos.x -= 1;
+	}
 EndDrawing();
 }
-UnloadTexture (birdy);
-UnloadTexture (sky);
+UnloadTexture(birdy);
+UnloadTexture(obstacle);
 CloseWindow();
 }
