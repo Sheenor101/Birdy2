f9c641ff2d9d7fd9b4b6241f6ede0c215d37f524
Sheenor101 <sheena_walsh@hotmail.com>
2022-11-10 19:13:42 +0000

    Birdy Final

--- birdy.cpp
+++ birdy.cpp
@@ -1,5 +1,7 @@
+// include the raylib library
 #include "raylib.h"
 
+// defining the struct/group
 struct Anim
 {
 	Rectangle rec; 
@@ -9,27 +11,32 @@ struct Anim
 	float runningTime;  
 };
 
+//main entry point
 int main(){
+//specifying the variables value - const int - it cannot be modified by programmer later on
  const int windowWidth{700};
  const int windowHeight{450};
+ //initialise the game window along with height, width and name
 InitWindow(windowWidth,windowHeight,"Birdy");
+//Inialise music
 InitAudioDevice();
-//const int width{50};
-//const int height{80};
 
 
+// load the different textures that will be used in the game
 Texture2D obstacle = LoadTexture("resources/Drone.png");
 Texture2D birdy = LoadTexture("resources/birdy.png");
 Texture2D sky = LoadTexture("resources/sky.png");
 
+//Defining the bird ainimation
 Anim birdyAnim;
+//
 birdyAnim.rec.width = birdy.width/6; 
 birdyAnim.rec.height = birdy.height;
 birdyAnim.rec.x = 0;
 birdyAnim.rec.y = 0;
-//starting position of player - x axis
+//starting position of bird - x axis
 birdyAnim.pos.x = windowWidth/6 - birdyAnim.rec.width/6;
-//starting posion of player - y axis
+//starting position of bird - y axis
 birdyAnim.pos.y = windowHeight/2 - birdyAnim.rec.height/2;
 birdyAnim.frame = 0;
 birdyAnim.updateTime = 1.0/12.0;
@@ -40,22 +47,23 @@ obRec.width = obstacle.width;
 obRec.height = obstacle.height; 
 obRec.x = 0;
 obRec.y = 0;
-//Vector2 obPos; 
+
 Vector2 obPos = { GetScreenWidth() / 2.0f, GetScreenHeight() / 2.0f }; 
 Vector2 obSpeed = { 7.0f, 5.0f };
 //obPos.x = windowWidth- obRec.width;
 //obPos.y = windowHeight - obRec.height;
 
+//defining the rectangle for the background sky
 Rectangle skyRec;
+//defining the width and height of the rectangle that the background will be in
 skyRec.width = sky.width*10;
 skyRec.height = sky.height;
+//the starting position on x and y axis of the background rectangle
 skyRec.x = 0;
 skyRec.y = 0;
 Vector2 skyPos;
 skyPos.x = windowWidth/2 - skyRec.width/2;
 skyPos.y = windowHeight - skyRec.height;
-
-
 
 
 //int posY{windowHeight-height};
@@ -200,7 +208,14 @@ birdyAnim.pos.y += velocity * deltaTime;
 obPos.x += obVel * deltaTime; 
 ClearBackground(WHITE); 
 	if (collision){DrawText("Birdy Died!!",290,200,30,BLACK);
-
+	// animate dead birdy dropping until off screen
+		if(birdyAnim.pos.y <= (GetScreenHeight())){
+// draw dead birdy 
+DrawTextureRec(birdy,birdyAnim.rec,birdyAnim.pos,RED);
+// draw obstacle
+DrawTextureRec(obstacle,obRec,obPos,WHITE);
+//birdy drops down 1 
+birdyAnim.pos.y += 1;}
 	}
 	else{
 
@@ -209,6 +224,11 @@ ClearBackground(WHITE);
 DrawTextureRec(sky, skyRec,skyPos, WHITE); skyPos.x -= 1;
 DrawTextureRec(birdy,birdyAnim.rec,birdyAnim.pos,WHITE);
 DrawTextureRec(obstacle,obRec,obPos,WHITE);
+
+// debug collision rectangles
+DrawRectangleLines(obPos.x, obPos.y, obRec.width, obRec.height, BLACK);
+DrawRectangleLines(birdyAnim.pos.x, birdyAnim.pos.y, birdyRec.width, birdyRec.height, BLACK);
+
 DrawText("Save Birdy from the Drone!",10,10,12,DARKGRAY);
 	}
 EndDrawing();
